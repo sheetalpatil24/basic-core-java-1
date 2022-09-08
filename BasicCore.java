763b2c5e1e546b9f14ff5634854de07d30a82f8b
sheetalpatil24 <patilsheetal7913@gmail.com>
2022-09-08 17:37:03 +0530

    swapping between two numbers

--- BasicCore.java
+++ BasicCore.java
@@ -1,26 +1,25 @@
-
+//swap numbers means altering each other,s values between two numbers
+//a=10 b=20  so a= 20 b=10
 import java.util.Scanner;
+
 public class BasicCore{
-
     public static void main(String[] args) {
-        
-        Scanner fac = new Scanner(System.in);
-      // n-vairable
-        int n ;
-        System.out.println("enter n");  
-         n = fac.nextInt();
-         //factors will be prime number
-         //d stands for divisor
 
-         for(int d=2 ; d < n; d += 1 )
-          {
-            while(n % d==0)
-            {
-                n= n/d;
-                System.out.println(d);
-            }
+    
+      
+        int a,b ,temp;
+        System.out.println("enter any two numbers");
+        Scanner r= new Scanner(System.in);
+        a = r.nextInt();
+        b= r.nextInt();
+        System.out.println("before swapping "+ a+" "+b);
+        // temporary variable
 
-            }
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println("after swapping "+ a+" "+b);
+
 
 
 
@@ -29,11 +28,3 @@ public class BasicCore{
 
 
 
-
-
-
-
-
-
-        
-    
