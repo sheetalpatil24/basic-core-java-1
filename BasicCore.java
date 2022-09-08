3674ca267484993ebac69258a898f99415d4b02e
sheetalpatil24 <patilsheetal7913@gmail.com>
2022-09-08 12:13:20 +0530

    harmonic series

--- BasicCore.java
+++ BasicCore.java
@@ -1,34 +1,28 @@
 import java.util.Scanner;
-
-
-
-
-//1.leap year century- gap between two leap years is 400 so after dividing it by 100 and 400 it will give reminder  is 0
-//2.yearly -after dividing by 100 and 4 will give reminder 0
-
-public class BasicCore{
+// n th harmonic number is the sum of reciprocals of the first n natural number
+ public class BasicCore{
     public static void main(String[] args) {
-        //year will be input here
-        int y;
-        System.out.println("enter any year");
-        //scanner class used to input value in runtime
-        //leap=object reference
-        Scanner leap = new Scanner(System.in);
-        y=leap.nextInt();
 
-        if(y%100==0 && y%400==0 || y%100!=0 && y%4==0 )
-        {
-            System.out.println("leap year");
-
-        }
-        else
-        {
-            System.out.println("not leap year");
+        System.out.println("enter the number");
+        Scanner n=new Scanner(System.in);
+        int num = n.nextInt();
         
+        //result will be in decimal so take variable  double
+        //N !=0 should greater than 0
+        
+            double result=0.0;
+            System.out.println("the harmonic series is");
+            while (num>0){
+                result = result +(double)1/num;
+                num--;
+            System.out.println(result +" , ");
+
+
         }
+        
     }
-}
-        
+ }
+
         
 
 
